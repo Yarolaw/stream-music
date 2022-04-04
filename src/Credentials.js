087fbec2743a63920937f705030d8260f8c927a1
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2022-04-04 19:46:11 +0300

    add Spotify Api

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '6ffea1a640ab413485d14ab128b5f7e9',
+        ClientSecret: 'c1c1a9362fa04de79489d5ba2376fcec'
+    }
+}
+
+export { Credentials };
